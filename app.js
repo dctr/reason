469a954f91a13e5f3b25c1d58515f46c2df304a9
David Christ <david.christ@uni-trier.de>
2013-03-05 15:34:11 +0100

    First display of cases

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-// Ready!
+// Setting up the environment
 require('./settings/express.js')(express, app, __dirname);
 require('./settings/template.js')(app);
 RSN = (function () {
   return {
-    datadir: __dirname + '/app_data'
+    datadir: __dirname + '/app_data',
+    exceptionToString: function (e) {
+      var str;
+      for (var prop in e) {
+        str += prop + ': ' + e.prop + '\n';
+      }
+      return str;
+    }
   }
 }());
 
-// Set!
+// Defining routes
 app.get(['/', '/home'], require('./routes/home.js'));
 app.get('/cases', require('./routes/cases.js'));
 app.get('/users', require('./routes/users.js'));
 app.get('/login', require('./routes/login.js'));
 
-// Go!
+// Start the server
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
--- routes/cases.js
+++ routes/cases.js
@@ -1,17 +1,34 @@
 module.exports = function (req, res) {
   var fs = require('fs');
 
-  var render = function (arguments) {
-    res.render('cases', arguments);
+  // Convenience
+  var pageContent = {};
+  var myRender = function () {
+    res.render('cases', pageContent);
   }
 
   try {
-    var message = fs.readdirSync(RSN.datadir);
-    if (typeof message === 'undefined') throw {
-      name: 'DirectoryReadError',
-      message: 'Could not read the given directory.'
+    var folders = fs.readdirSync(RSN.datadir + '/cases');
+    var file
+    pageContent.cases = {};
+    for (var i in folders) {
+      // TODO This could easly be made async
+      file = fs.readFileSync(RSN.datadir + '/cases/' + folders[i] +
+                             '/description.json', 'utf-8');
+      file = JSON.parse(file);
+      pageContent.cases[folders[i]] = {
+        id: folders[i],
+        creationDate: file.creationDate,
+        summary: file.summary
+      };
     }
   } catch (e) {
-    render({error: e.name + ': ' + e.message});
+    console.log(e);
+    for (var prop in e) {
+      console.log(prop + ': ' + e.prop);
+    }
+    pageContent.error = "1";
   }
+
+  myRender();
 };
