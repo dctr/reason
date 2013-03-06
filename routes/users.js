4523655c97dcc51c6d9c73c1dc7c72fbbb1af40f
David Christ <david.christ@uni-trier.de>
2013-03-06 11:20:55 +0100

    baka

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 module.exports = function (req, res) {
+  var fs = require('fs');
 
   // Convenience
   var pageContent = {};
@@ -6,15 +7,23 @@ module.exports = function (req, res) {
     res.render('users', pageContent);
   }
 
-  pageContent.users = {
-    foo: {
-      tags: ['nice', 'pretty', 'handsome'],
-      cases: [1, 2]
-    },
-    bar: {
-      tags: ['handsome', 'fairly usefull'],
-      cases: [1, 3]
+  try {
+    var folders = fs.readdirSync(RSN.datadir + '/users');
+    var file
+    pageContent.users = {};
+    for (var i in folders) {
+      // TODO This could easly be made async
+      file = fs.readFileSync(RSN.datadir + '/users/' + folders[i] +
+                             '/description.json', 'utf-8');
+      file = JSON.parse(file);
+      pageContent.users[folders[i]] = {
+        id: folders[i],
+        tags: file.tags,
+        cases: file.cases
+      };
     }
+  } catch (e) {
+    pageContent.error = 'Somethings wrong in the users database.';
   }
 
   myRender();
