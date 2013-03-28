a95705eb1274a6edf3be71402fa226f49d96f8e0
David Christ <david.christ@uni-trier.de>
2013-03-28 15:31:26 +0100

    Built in client-side templating via underscore.js

--- scripts/global.js
+++ scripts/global.js
@@ -1,10 +1,26 @@
-/*global console */
-/*jslint browser: true, indent: 2 */
+/**
+ * Global functions
+ *
+ * In this file, functions within the global RSN object are defined.
+ * However, the object is further augmented in other files!
+ */
+/*jslint browser: true, indent: 2, nomen: true */
+/*global $, _, console */
 (function () {
   'use strict';
 
-  var RSN = {};
+  var RSN, compileTemplate;
 
+  RSN = {};
+
+  // Compiles templates and memoizes them.
+  compileTemplate = _.memoize(function (template) {
+    var tpl = $('script.' + template).html();
+    console.log(tpl);
+    return _.template(tpl);
+  });
+
+  // BEGIN DEBUG METHODS
   RSN.populateStorage = function () {
     sessionStorage.foo = "bar";
     localStorage.ba = "ka";
@@ -17,18 +33,28 @@
     console.log('LOCAL STORAGE');
     console.log(RSN.stringify(localStorage));
   };
+  // END DEBUG METHODS
 
   RSN.parse = function (string) {
     return JSON.parse(string);
   };
 
+  RSN.render = function (template, data) {
+    console.log(template + ' -> ' + RSN.stringify(data));
+    var fun = compileTemplate(template);
+    $('div[role="main"]').html(fun(data));
+  };
+
+  /**
+   * Convenience method to get a formated JSON string.
+   * @param  {object} object A javascript object.
+   * @return {string}        The stringified JSON object.
+   */
   RSN.stringify = function (object) {
     return JSON.stringify(object, null, 2);
   };
 
-  /**
-   * Export RSN to either a global browser object or as node module;
-   */
+  // Export RSN to either a window browser object or as node module;
   /*jslint browser:true, node: true */
   if (window) {
     window.RSN = RSN;
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,33 @@
-/*global RSN, console */
-/*jslint browser: true, indent: 2 */
+/**
+ * Index:
+ *
+ * - Definition of all vars
+ * - Initialization of all data vars
+ * - Initialization of all function vars
+ * - Registration of event handlers
+ * - Script code
+ */
+/*global $, EJS, RSN, console */
+/*jslint browser: true, indent: 2, todo: true */
 (function () {
   'use strict';
+
+  var siteChange;
+
+  siteChange = function (newSite) {
+    // TODO: Render the template for newSite.
+    // DEBUG:
+    newSite = 'home';
+    RSN.render(newSite, {message: 'hello world'});
+  };
+
+  $(document).ready(function () {
+    // Functionality for the nav bar.
+    $('nav a').click(function (e) {
+      e.preventDefault();
+      $('nav a').attr('class', '');
+      $(this).attr('class', 'selected');
+      siteChange($(this).html());
+    });
+  });
 }());
