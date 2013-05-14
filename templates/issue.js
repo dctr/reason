cd3a089b20566ab00a92e6813f438b187467d297
David Christ <david.christ@uni-trier.de>
2013-05-14 12:15:31 +0200

    Added mockup for issues GitChat graph

--- templates/issue.js
+++
@@ -1,7 +0,0 @@
-/*jslint browser: true, indent: 2, nomen: true, todo: true */
-/*global $, _, RSN, TPL, console */
-TPL.cacheScript('issue', function (data, render) {
-  'use strict';
-
-  render(data);
-});
