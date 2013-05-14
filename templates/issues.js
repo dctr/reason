e854a360beedbe6eaf072d6f39a96e4485b1064c
David Christ <david.christ@uni-trier.de>
2013-05-14 09:20:00 +0200

    Cleaned up issues.js

--- templates/issues.js
+++ templates/issues.js
@@ -12,44 +12,31 @@ TPL.cacheScript('issues', function (data, render) {
   // Get list of branches == issues
   // - Title, time opened, time of last activity, opened/closed-status, tags
 
-  var asyncs, asyncRender, repo;
+  var repo;
 
-  asyncs = [];
   repo = GHB.getRepo('issuetracker', data.repo.replace('/', '___'));
 
-  asyncs.push(function () {
-    // Get a list of branch names.
-    repo.listBranches(function (err, branches) {
-      var b, asyncGetRepos;
-      b = [];
-      // After all branches are fetched, store data sorted by date and continue.
-      asyncGetRepos = _.after(branches.length, function () {
-        data.branches = _.sortBy(b, function (elem) {
-          return new Date(elem.committer.date).getTime();
-        });
-        asyncRender();
+  repo.listBranches(function (err, branches) {
+    var b, _afterGotBranches;
+    b = [];
+    // After all branches are fetched, store data sorted by date and continue.
+    _afterGotBranches = _.after(branches.length, function () {
+      data.branches = _.sortBy(b, function (elem) {
+        return new Date(elem.committer.date).getTime();
       });
-      // Get commit object for each branch's head.
-      _.each(branches, function (branch) {
-        repo.getBranch(branch, function (err, data) {
-          data.name = branch;
-          b.push(data);
-          asyncGetRepos();
-        });
+      render(data);
+      $('div[role="main"] a').click(function (e) {
+        e.preventDefault();
+        TPL.render('issue', {issue: $(this).attr('id')});
+      });
+    });
+    // Get commit object for each branch's head.
+    _.each(branches, function (branch) {
+      repo.getBranch(branch, function (err, data) {
+        data.name = branch;
+        b.push(data);
+        _afterGotBranches();
       });
     });
   });
-
-  asyncRender = _.after(asyncs.length, function () {
-    render(data);
-    $('div[role="main"] a').click(function (e) {
-      e.preventDefault();
-      TPL.render('issue', {issue: $(this).attr('id')});
-    });
-  });
-
-  _.each(asyncs, function (fn) {
-    fn();
-  });
-
 });
