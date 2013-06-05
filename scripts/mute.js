cb32af24e882dc2c5670655e22a7ffea009105f1
David Christ <david.christ@uni-trier.de>
2013-06-05 15:05:01 +0200

    Added JSDoc to mute.js

--- scripts/mute.js
+++ scripts/mute.js
@@ -17,7 +17,7 @@
 
   /**
    * Cache a script with a given name
-   * @param  {string} template The name of the template.
+   * @param  {string}   template The name of the template.
    * @param  {function} script   The function to be executed on template execution.
    */
   muteScript = function (template, script) {
@@ -31,7 +31,16 @@
     cachedScripts[template] = script;
   };
 
-
+  /**
+   * Function to export as global mute object
+   *
+   * This is a function creating a template engine instance for a certain CSS selector.
+   * It is enrichted by "static" functions below.
+   * @param  {string} selector A CSS selector defining the Element which's content should be replaced by a template.
+   * @param  {string} ejsDir   URL path which contains the EJS files.
+   * @param  {string} jsDir    URL path which contains the JS files.
+   * @return {object}          A mute instance for the CSS selector.
+   */
   constructor = function (selector, ejsDir, jsDir) {
     var that, back, currentContent, forth, noBackForth, redirects, renderCompiledTemplate;
 
@@ -63,6 +72,11 @@
     that = {};
 
     // TODO: Is it possible to use the browser (e.g. document.history.push/pop)?
+    /**
+     * Replace the current page with the previous one.
+     * @todo Can you use the browsers history for single page apps?
+     * @return {number} The number of pages in the backwards log.
+     */
     that.backwards = function () {
       var upcomming = back.pop();
       if (!upcomming) { return; }
@@ -72,6 +86,11 @@
       return back.length;
     };
 
+    /**
+     * Replace the current page with the previous one after a backwards.
+     * @todo Can you use the browsers history for single page apps?
+     * @return {number} The number of pages in the forwards log.
+     */
     that.forwards = function () {
       var upcomming = forth.pop();
       if (!upcomming) { return; }
@@ -81,6 +100,11 @@
       return forth.length;
     };
 
+    /**
+     * Render a template
+     * @param  {string} template Template name (also filename without extension).
+     * @param  {object} data     A data object which will be present within the corresponding script and the template.
+     */
     that.render = function (template, data) {
       var ex, reqTpl;
 
@@ -126,6 +150,11 @@
       }
     };
 
+    /**
+     * Sets an alias for a certain template
+     * @param  {string} source The templates name.
+     * @param  {string} target Which template should be displayed instead
+     */
     that.setRedirect = function (source, target) {
       if (!/^[A-Za-z0-9]*$/.test(source) ||
           !/^[A-Za-z0-9]*$/.test(target)) {
@@ -140,6 +169,11 @@
     return that;
   };
 
+  /**
+   * Convert <br>, <br/>, and <br /> to \n
+   * @param  {string} str The source string.
+   * @return {string}     The processed string
+   */
   constructor.br2nl = function (str) {
     if (typeof str !== 'string') {
       throw {
@@ -150,11 +184,23 @@
     return str.replace(/<br\s*\/?>/mg, '\n');
   };
 
+  /**
+   * Clear the template and script cache
+   *
+   * Usefull, if the templates/scripts change server-side.
+   */
   constructor.clear = function () {
     cachedScripts = {};
     cachedTemplates = {};
   };
 
+  /**
+   * Convert \n to <br />
+   *
+   * Using self-closing br tag to be compatible with both HTML5 _and_ XHTML.
+   * @param  {string} str The source string.
+   * @return {string}     The processed string
+   */
   constructor.nl2br = function (str) {
     if (typeof str !== 'string') {
       throw {
@@ -162,7 +208,6 @@
         message: 'Invalid call to nl2br().'
       };
     }
-    // Using self-closing tag to be compatible with HTML5 _and_ XHTML.
     var breakTag = '<br />';
     return str.replace(
       /(\r\n|\n\r|\r|\n)/mg,
@@ -170,6 +215,8 @@
     );
   };
 
-
+  /**
+   * Export the global mute object.
+   */
   window[modulename] = constructor;
 }('mute'));
